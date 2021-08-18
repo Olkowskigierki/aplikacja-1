365082d8a5f1d2f28506deea34bd7bbd37e72416
Olek <Olek@DESKTOP-RQC873R>
2021-08-18 19:42:04 +0200

    Dodaj pliki projektów.

---
+++ aplikacja 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj w aplikacji Asystent 1.0.0 autorstwa Aleksandera Kuligowskiego");
+        }
+    }
+}
